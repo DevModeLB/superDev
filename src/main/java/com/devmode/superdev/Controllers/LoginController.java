047a90040c889f227bcc56ebb20cc01a84a0cecc
Hussein Nahal <nahalhusssein1000@gmail.com>
2024-08-24 11:52:41 +0300

    add products and suppliers

--- src/main/java/com/devmode/superdev/Controllers/LoginController.java
+++ src/main/java/com/devmode/superdev/Controllers/LoginController.java
@@ -98,7 +98,7 @@ public class LoginController {
     private void switchView() {
         try {
             // Load the Category FXML file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/categories/addCategory.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/products/addProduct.fxml"));
             Parent categoryView = loader.load();
             // Get the current stage
             Stage stage = (Stage) loginButton.getScene().getWindow();
