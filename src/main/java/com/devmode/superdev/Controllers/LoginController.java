dec7015ed26c4e665bb08a885dd362f4ec833c6d
Hussein Nahal <nahalhusssein1000@gmail.com>
2024-08-26 22:07:59 +0300

    Added categories in the home page and edit pages

--- src/main/java/com/devmode/superdev/Controllers/LoginController.java
+++ src/main/java/com/devmode/superdev/Controllers/LoginController.java
@@ -98,7 +98,7 @@ public class LoginController {
     private void switchView() {
         try {
             // Load the Category FXML file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/products/getProducts.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/home.fxml"));
             Parent categoryView = loader.load();
             // Get the current stage
             Stage stage = (Stage) loginButton.getScene().getWindow();
