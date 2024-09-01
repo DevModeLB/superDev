5421dbf27028d75322f7aa330b253f0700b5193f
talebof <talebof@hotmail.com>
2024-09-01 12:46:12 +0300

    improve

--- src/main/java/com/devmode/superdev/Controllers/EditProductController.java
+++ src/main/java/com/devmode/superdev/Controllers/EditProductController.java
@@ -15,7 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.application.Platform;
-import javafx.scene.Scene;
+
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
